287dede201aaace3b7fef471796cc801b60d12cf
Thibault Cheneviere <thibault.chene23@gmail.com>
2025-01-11 18:50:18 +0100

    feat: added highlight class, darkmode setup and gsap for animation purpose (maybe removable)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+	import ThemeInitializer from '$lib/components/themeInitializer.svelte';
+
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
-<main class="mx-auto max-w-5xl pt-24">
-	{@render children()}
-</main>
+<ThemeInitializer>
+	<header
+		class="dark:bg-dark-background fixed inset-x-0 top-0 z-10
+       bg-white"
+	>
+		<nav
+			class="mx-auto flex h-24 max-w-5xl
+ items-center justify-between"
+		>
+			<span>Thibault Cheneviere</span>
+			<div class="flex items-center space-x-10">
+				<a href="#about">About</a>
+				<a href="todo">Blog</a>
+			</div>
+		</nav>
+	</header>
+
+	<main class="mx-auto max-w-5xl pt-24">
+		{@render children()}
+	</main>
+</ThemeInitializer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<section id="about">
+	<h1>
+		Hello! I'm Thibault a <span
+			id="job"
+			class="highlight highlight-animate highlight-spread-sm after:bg-indigo-500"
+			>Software Engineer</span
+		> based in Melbourne
+	</h1>
+</section>
