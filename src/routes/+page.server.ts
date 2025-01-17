60af1f6e26a53a4fd71107ceee678b8d3b94be13
Thibault Cheneviere <thibault.chene23@gmail.com>
2025-01-17 01:48:22 +0100

    feat: added a local cache for the pinned repos to avoid fetching over and over again

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,23 @@
 import type { PageServerLoad } from './$types';
 import { get_repo } from '$lib/server/utils/github';
+import Keyv from 'keyv';
+import type { Repository } from '$lib/types';
 
-// get url path whenever visiting a new page
-export const load: PageServerLoad = async () => {
+const cache: Keyv<Repository[]> = new Keyv();
+
+// We can cache the data for 1 hour as it is not going to change (the pinned repositories)
+export const load: PageServerLoad = async ({ setHeaders }) => {
+	setHeaders({ 'cache-control': 'public, max-age=3600000' });
+
+	if (await cache.has('repos')) {
+		const repos = (await cache.get('repos')) as Repository[];
+		return {
+			repos
+		};
+	}
+
 	const repos = await get_repo();
+	await cache.set('repos', repos, 3600000);
 
 	return {
 		repos
